3ef933960f89c01247cc4ceef7383da89f59ffc8
Raphaël BATICLE <61377616+Lasauce6@users.noreply.github.com>
2023-10-01 14:07:44 +0200

    fix header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,115 +6,120 @@ const siteTitle = 'Raphaël Baticle'
  */
 export default defineNuxtConfig({
 
-  /**
-   * * App Config
-   * app config: https://nuxt.com/docs/api/configuration/nuxt-config#app
-   * head config: https://nuxt.com/docs/api/configuration/nuxt-config#head
-   * meta config: https://nuxt.com/docs/getting-started/seo-meta
-   * pageTransition config: https://nuxt.com/docs/getting-started/transitions#transitions
-   * TODO: Add more meta tags for SEO
-   * TODO: Add tags for social media sharing
-   * TODO: Migrate apple-touch-icon config to manifest.json
-   */
-  devtools: {
-   enabled: true,
+    /**
+     * * App Config
+     * app config: https://nuxt.com/docs/api/configuration/nuxt-config#app
+     * head config: https://nuxt.com/docs/api/configuration/nuxt-config#head
+     * meta config: https://nuxt.com/docs/getting-started/seo-meta
+     * pageTransition config: https://nuxt.com/docs/getting-started/transitions#transitions
+     * TODO: Add more meta tags for SEO
+     * TODO: Add tags for social media sharing
+     * TODO: Migrate apple-touch-icon config to manifest.json
+     */
+    devtools: {
+        enabled: true,
 
-   timeline: {
-    enabled: true
-   }
-  },
-  app: {
-    head: {
-      title: siteTitle, // App window nav title
-
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'theme-color', content: '#121212' },
-        // ...
-      ],
-      link: [
-        { rel: 'manifest', href: 'pwa/manifest.json' },
-        { rel: 'apple-touch-icon', href: 'pwa/icons/apple-touch-icon.png' },
-      ],
+        timeline: {
+            enabled: true
+        }
     },
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
+    app: {
+        head: {
+            title: siteTitle, // App window nav title
 
-  /**
-   * * Nuxt 3 Modules
-   * Official modules: https://nuxt.com/modules
-   */
-  modules: [
-    'nuxt-icon',
-    '@nuxtjs/i18n',
-    '@nuxt/content',
-    '@nuxtjs/color-mode',
-    '@nuxtjs/tailwindcss',
-  ],
+            meta: [
+                {charset: 'utf-8'},
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+                {name: 'theme-color', content: '#121212'},
+                // ...
+            ],
+            link: [
+                {rel: 'manifest', href: 'pwa/manifest.json'},
+                {rel: 'apple-touch-icon', href: 'pwa/icons/apple-touch-icon.png'},
+            ],
+        },
+        pageTransition: {name: 'page', mode: 'out-in'}
+    },
 
-  components: {
-    dirs: [
-      '~/components',
-      '~/components/library'
+    /**
+     * * Nuxt 3 Modules
+     * Official modules: https://nuxt.com/modules
+     */
+    modules: [
+        'nuxt-icon',
+        '@nuxtjs/i18n',
+        '@nuxt/content',
+        '@nuxtjs/color-mode',
+        '@nuxtjs/tailwindcss',
     ],
-  },
 
-  /**
-   * * Tailwind CSS Config
-   * Options: https://tailwindcss.nuxt.dev/getting-started/options/
-   * Docs: https://tailwindcss.nuxt.dev
-   */
-  tailwindcss: {
-    cssPath: '~/assets/tailwind.css',
-    configPath: 'tailwind.config',
-    exposeConfig: true, // true to resolve the tailwind config in runtime. https://tailwindcss.nuxt.dev/getting-started/options/#exposeconfig
-    injectPosition: 0,
-    viewer: true, // set up the /_tailwind/ route. (Disable in production) https://tailwindcss.nuxt.dev/getting-started/options/#viewer
-  },
+    components: {
+        dirs: [
+            '~/components',
+            '~/components/library'
+        ],
+    },
 
-  /**
-   * * i18n Config
-   * Official module: https://nuxt.com/modules/i18n
-   */
-  i18n: {
-    defaultLocale: 'fr',
-    detectBrowserLanguage: false,
-    langDir: 'lang/',
-    lazy: true,
-    locales: [
-      {
-        code: 'fr',
-        file: 'fr.json',
-        iso: 'fr-FR',
-        name: 'Francais',
-      },
-      {
-        code: 'en',
-        file: 'en.json',
-        iso: 'en-US',
-        name: 'English',
-      },
-  ]},
+    /**
+     * * Tailwind CSS Config
+     * Options: https://tailwindcss.nuxt.dev/getting-started/options/
+     * Docs: https://tailwindcss.nuxt.dev
+     */
+    tailwindcss: {
+        cssPath: '~/assets/tailwind.css',
+        configPath: 'tailwind.config',
+        exposeConfig: true, // true to resolve the tailwind config in runtime. https://tailwindcss.nuxt.dev/getting-started/options/#exposeconfig
+        injectPosition: 0,
+        viewer: true, // set up the /_tailwind/ route. (Disable in production) https://tailwindcss.nuxt.dev/getting-started/options/#viewer
+    },
 
-  /**
-   * * Color mode Config
-   * Official module: https://nuxt.com/modules/color-mode
-   */
-  colorMode: {
-    classSuffix: '',
-  },
+    /**
+     * * i18n Config
+     * Official module: https://nuxt.com/modules/i18n
+     */
+    i18n: {
+        defaultLocale: 'fr',
+        detectBrowserLanguage: {
+            useCookie: true,
+            cookieKey: 'i18n_redirected',
+            redirectOn: 'root'
+        },
+        langDir: 'lang/',
+        lazy: true,
+        locales: [
+            {
+                code: 'fr',
+                file: 'fr.json',
+                iso: 'fr-FR',
+                name: 'Francais',
+            },
+            {
+                code: 'en',
+                file: 'en.json',
+                iso: 'en-US',
+                name: 'English',
+            },
+        ]
+    },
 
-  /**
-   * * Runtime Config (Environment Variables)
-   * Usage: https://nuxt.com/docs/guide/going-further/runtime-config
-   */
-  runtimeConfig: {
-    // The private keys which are only available server-side
-    apiSecret: '123',
-    // Keys within public are also exposed client-side
-    public: {
-      apiBase: '/api'
+    /**
+     * * Color mode Config
+     * Official module: https://nuxt.com/modules/color-mode
+     */
+    colorMode: {
+        classSuffix: '',
+    },
+
+    /**
+     * * Runtime Config (Environment Variables)
+     * Usage: https://nuxt.com/docs/guide/going-further/runtime-config
+     */
+    runtimeConfig: {
+        // The private keys which are only available server-side
+        apiSecret: '123',
+        // Keys within public are also exposed client-side
+        public: {
+            apiBase: '/api'
+        }
     }
-  }
 })
